60527698653e5f9fe6d317902d531772703d5f8f
amrutapatil9699 <amrutapatil9699@gmail.com>
2025-06-13 17:40:26 +0530

    fourthcommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Pages
@@ -12,12 +12,28 @@ import CoursesPage from './pages/CoursesPage';
 
 import NotFoundPage from './pages/NotFoundPage';
 import ChatbotComponent from './components/Chatbot/ChatbotComponents';
+import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
 
 function App() {
+  const [showPopup, setShowPopup] = useState(true);
+
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
   return (
+    <> 
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Amruta Bhagwan Patil"
+          studentPhotoUrl="\Images\amrutapatil.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div>
     <Router>
      
-      
       <main style={{ padding: '1rem' }}>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -30,6 +46,7 @@ function App() {
       </main>
     <ChatbotComponent/>
     </Router>
+    </>
   );
 }
 
